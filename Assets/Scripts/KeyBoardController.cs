22bbd0f2812b8d06b6f6700cbe2bcede94feafc6
taylorjames9 <taylorjames9@gmail.com>
2016-07-12 18:24:16 -0700

    first commit by jt

---
+++ Assets/Scripts/KeyBoardController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBoardController : MonoBehaviour {
+
+
+	float maxSpeed = 20.0f;
+	float accel = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Rigidbody> ().velocity.magnitude >= maxSpeed)
+			return;
+			
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+			StartCoroutine (ChangeDirection ("Up"));
+		if (Input.GetKeyDown (KeyCode.DownArrow))
+			StartCoroutine (ChangeDirection ("Down"));
+		if (Input.GetKeyDown (KeyCode.RightArrow))
+			StartCoroutine (ChangeDirection ("Right"));
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+			StartCoroutine (ChangeDirection ("Left"));
+	
+	}
+
+
+	IEnumerator ChangeDirection(string command ){
+		GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		switch (command) {
+		case "Up":
+			GetComponent<Rigidbody>().AddRelativeForce(Vector3.up*accel);
+			break;
+		case "Down":
+			GetComponent<Rigidbody>().AddRelativeForce(Vector3.down*accel);
+			break;
+		case "Right":
+			GetComponent<Rigidbody>().AddRelativeForce(Vector3.right*accel);
+			break;
+		case "Left":
+			GetComponent<Rigidbody>().AddRelativeForce(Vector3.left*accel);
+			break;
+
+		}
+		yield return 0;
+	}
+}
---
+++ Assets/Scripts/Trail_Renderer_Movement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trail_Renderer_Movement : MonoBehaviour {
+
+
+	float maxSpeed = 5.0f;
+	float accel = 0.1f;
+	bool changingDirection = false;
+
+	void FixedUpdate () {
+		if(GetComponent<Rigidbody>().velocity.magnitude <= maxSpeed)
+			GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward*accel);
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Trigger Enter");
+		if (other.tag.Equals ("Direction") && !changingDirection) {
+			StartCoroutine ("ChangeDirection");
+		}
+	}
+
+	IEnumerator ChangeDirection( ){
+		changingDirection = true;
+		yield return new WaitForSeconds(0.25f);
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0.01f, 0.01f, 0.0f);
+		int rand = UnityEngine.Random.Range (0, 3);
+		int rand_interpret = 0;
+		switch (rand) {
+		case 0:
+			rand_interpret = -90;
+			break;
+		case 1:
+			rand_interpret = 90;
+			break;
+		case 2:
+			rand_interpret = 180;
+			break;
+		case 3:
+			rand_interpret = 45;
+			break;
+
+		}
+
+		transform.Rotate (rand_interpret, 0, 0);
+		yield return new WaitForSeconds(1.0f);
+
+		changingDirection = false;
+
+	}
+
+}
